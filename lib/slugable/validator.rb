1d1a6341feac993803358a218f71195b7673dfc5
Samuel Molnar <molnar.samuel@gmail.com>
2013-08-20 13:16:39 +0200

    Add validation error messages

--- lib/slugable/validator.rb
+++ lib/slugable/validator.rb
@@ -4,11 +4,21 @@ module Slugable
       base  = record.class
       value = record.read_attribute(to)
 
-      return unless value.present?
+      unless value.present?
+        record.errors.add(to, :blank)
+
+        return false
+      end
 
       other = base.where(to => value).first
 
-      other.nil? || other.respond_to?(:parent_id) && other.parent_id != record.parent_id
+      if other.nil? || (other.respond_to?(:parent_id) && other.parent_id != record.parent_id)
+        return true
+      else
+        record.errors.add(to, :not_unique)
+
+        return false
+      end
     end
   end
 end
--- spec/slugable/validator_spec.rb
+++ spec/slugable/validator_spec.rb
@@ -4,25 +4,30 @@ describe Slugable::Validator do
   let(:base)      { double(:base) }
   let(:record)    { double(:record) }
   let(:validator) { described_class }
+  let(:errors)    { double(:errors) }
 
   describe '#validate' do
     context 'when validating slug presence' do
       it 'should not mark record as valid if missing slug value' do
         record.should_receive(:class).and_return(base)
         record.should_receive(:read_attribute).with(:slug).and_return('')
+        record.should_receive(:errors).and_return(errors)
+        errors.should_receive(:add).with(:slug, :blank)
 
         validator.validate(record, :slug).should be_false
       end
     end
 
     context 'when validating uniqueness' do
-      it 'should not mark record as valid already exists' do
+      it 'should not mark record as valid already if exists' do
         other = double(:other)
 
         record.should_receive(:class).and_return(base)
         record.should_receive(:read_attribute).with(:slug).and_return('value')
         base.should_receive(:where).with(:slug  => 'value').and_return([other])
         other.should_receive(:respond_to?).with(:parent_id).and_return(false)
+        record.should_receive(:errors).and_return(errors)
+        errors.should_receive(:add).with(:slug, :not_unique)
 
         validator.validate(record, :slug).should be_false
       end
